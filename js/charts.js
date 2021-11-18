017213cb7c45a3cc6430cd0b9389a300023bf826
AlexEastwood <76944389+AlexEastwood@users.noreply.github.com>
2021-11-18 15:15:25 +0000

    First chart underway

--- js/charts.js
+++ js/charts.js
@@ -1,35 +1,34 @@
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
+const trafficChartID = document.getElementById('trafficChart');
+
+let trafficData = {
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+    datasets: [{
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500], 
+        backgroundColor: 'rgba(116, 119, 191, .3)',
+        borderWidth: 1,
+        tension: 0.5,
+        fill: true,
+    }]
+};
+
+let trafficOptions = {
+    aspectRatio: 2.5,
+    maintainAspectRatio: true,
+    scales: {
+        y: {
+            beginAtZero: true
         }
-    }
+    },
+    plugins: {
+        legend: {
+        display: false
+        }
+    },
+    responsive: true
+};
+
+let trafficChart = new Chart(trafficChartID, {
+    type: 'line',
+    data: trafficData,
+    options: trafficOptions
 });
