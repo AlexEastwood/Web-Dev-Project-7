911910c1a258ace48ed829521da442205c802a7d
AlexEastwood <76944389+AlexEastwood@users.noreply.github.com>
2021-11-19 16:02:52 +0000

    Reworking header for 320px

--- js/charts.js
+++ js/charts.js
@@ -4,7 +4,7 @@ let trafficData = {
     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
     datasets: [{
         data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500], 
-        backgroundColor: 'rgba(116, 119, 191, .3)',
+        backgroundColor: 'rgb(116,119,191,.3)',
         borderWidth: 1,
         tension: 0.5,
         fill: true,
