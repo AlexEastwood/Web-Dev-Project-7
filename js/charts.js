bcb7f728a4735cd25675db8f9564e8fadcc6ef52
AlexEastwood <76944389+AlexEastwood@users.noreply.github.com>
2021-11-23 14:47:01 +0000

    Added bar chart

--- js/charts.js
+++ js/charts.js
@@ -1,4 +1,5 @@
 const trafficChartID = document.getElementById('trafficChart');
+const dailyTrafficChartID = document.getElementById('dailyTrafficChart');
 
 let trafficData = {
     labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
@@ -10,6 +11,14 @@ let trafficData = {
         fill: true,
     }]
 };
+
+let dailyTrafficData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+        data: [75, 110, 175, 125, 225, 200, 100],
+        backgroundColor: "rgb(116,119,191)"
+    }]
+}
 
 let trafficOptions = {
     aspectRatio: 2.5,
@@ -28,5 +37,30 @@ let trafficOptions = {
 let trafficChart = new Chart(trafficChartID, {
     type: 'line',
     data: trafficData,
-    options: trafficOptions
+    options: {
+        aspectRatio: 2.5,
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        },
+        plugins: {
+            legend: {
+            display: false
+            }
+        }
+    }
+});
+
+let dailyTrafficChart = new Chart(dailyTrafficChartID, {
+    type: 'bar',
+    data: dailyTrafficData,
+    options: {
+        aspectRatio: 2.5,
+        plugins: {
+            legend: {
+            display: false
+            }
+        }
+    }
 });
